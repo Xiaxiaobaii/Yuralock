e8b67bc778b00be85141814308052db66a5cbe1b
Xiaxiaobaii <shizheqwq@gmail.com>
2026-04-02 00:36:51 +0800

    update style, update file format, android todo

---
+++ src-tauri/gen/android/buildSrc/src/main/java/com/xiaxiaobai/yuralock/kotlin/BuildTask.kt
@@ -0,0 +1,72 @@
+import java.io.File
+import javax.inject.Inject
+import org.apache.tools.ant.taskdefs.condition.Os
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.logging.LogLevel
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+import org.gradle.process.ExecOperations
+
+open class BuildTask @Inject constructor(
+    private val execOperations: ExecOperations
+) : DefaultTask() {
+    @Input
+    var rootDirPath: String? = null
+    @Input
+    var target: String? = null
+    @Input
+    var release: Boolean? = null
+
+    @TaskAction
+    fun assemble() {
+        val executable = """npm"""
+        try {
+            runTauriCli(executable)
+        } catch (e: Exception) {
+            if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+                // Try different Windows-specific extensions
+                val fallbacks = listOf(
+                    "$executable.exe",
+                    "$executable.cmd",
+                    "$executable.bat",
+                )
+                
+                var lastException: Exception = e
+                for (fallback in fallbacks) {
+                    try {
+                        runTauriCli(fallback)
+                        return
+                    } catch (fallbackException: Exception) {
+                        lastException = fallbackException
+                    }
+                }
+                throw lastException
+            } else {
+                throw e
+            }
+        }
+    }
+
+    fun runTauriCli(executable: String) {
+        val rootDirPath = rootDirPath ?: throw GradleException("rootDirPath cannot be null")
+        val target = target ?: throw GradleException("target cannot be null")
+        val release = release ?: throw GradleException("release cannot be null")
+        val args = listOf("run", "--", "tauri", "android", "android-studio-script")
+
+        execOperations.exec {
+            workingDir(File(rootDirPath))
+            executable(executable)
+            args(args)
+            if (logger.isEnabled(LogLevel.DEBUG)) {
+                args("-vv")
+            } else if (logger.isEnabled(LogLevel.INFO)) {
+                args("-v")
+            }
+            if (release) {
+                args("--release")
+            }
+            args(listOf("--target", target))
+        }.assertNormalExitValue()
+    }
+}
